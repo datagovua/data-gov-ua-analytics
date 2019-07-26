10555436498c02238275d1a10ebf6a7040392d51
vanuan <vanuan@gmail.com>
2019-07-26 18:41:27 +0300

    Make sure indexes are created before use

--- app/triggerCrawlMetadata.js
+++ app/triggerCrawlMetadata.js
@@ -10,39 +10,39 @@ const url = require('url');
 const crawlMetadata = require('./crawlMetadata');
 const parser = require('./parser');
 const createDbReader = require('./dbReader');
+const createDbSaver = require('./dbSaver');
 
 const ONLY_NODES = process.env.ONLY_NODES && process.env.ONLY_NODES.split(',').map((i) => parseInt(i, 10));
 
-module.exports = function() {
+module.exports = async function() {
   let reader;
+  let saver;
 
-  function readRevisions() {
-    reader = createDbReader();
-    return reader.init().then(() => reader.readTempRevisions(ONLY_NODES));
-  }
+  saver = createDbSaver();
+  await saver.init();
 
-  return readRevisions()
-  .then(revisionsCursor => {
-    return revisionsCursor.eachAsync((revision) => {
-      // skip revisions without dataset_id
-      if(!revision.dataset_id) {
-        return;
+  reader = createDbReader();
+  await reader.init();
+
+  const revisionsCursor = await reader.readTempRevisions(ONLY_NODES);
+
+  await revisionsCursor.eachAsync(async (revision) => {
+    // skip revisions without dataset_id
+    if(!revision.dataset_id) {
+      return;
+    }
+    const errors = await crawlMetadata([revision]);
+    if(errors.length) {
+      console.log("Permanent errors:")
+    }
+    errors.forEach(function(e) {
+      if(e.statusCode !== undefined) {
+        console.log(e.path, e.statusCode);
+      } else {
+        console.log(e);
       }
-      return crawlMetadata([revision]).then(errors => {
-        if(errors.length) {
-          console.log("Permanent errors:")
-        }
-        errors.forEach(function(e) {
-          if(e.statusCode !== undefined) {
-            console.log(e.path, e.statusCode);
-          } else {
-            console.log(e);
-          }
-        });
-      });
-    })
-  })
-  .then(() => {
-    return reader.finish();
+    });
   });
+  await reader.finish();
+  await saver.finish();
 }
